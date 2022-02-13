21ccb15ec25764edac93f50574aa41f00fbe586b
manikanta324 <55922779+manikanta324@users.noreply.github.com>
2022-02-13 17:22:45 +0530

    Deleted the entire existing code

--- c1.cpp
+++ c1.cpp
@@ -3,45 +3,10 @@ using namespace std;
 
 int main()
 {
-   ifstream indata;
-   int num;
-   indata.open("test1.txt");
-   int column=0;
-   if(!indata) 
-   { 
-      cerr << "Error: file could not be opened" << endl;
-      exit(1);
-   }
    
-   indata >> num;
-   
-   while ( !indata.eof() ) 
-   {
-      cout << "\nThe next number is " << num << endl;
-      indata >> num;
-	column++; 
-   }
+   // DELETED existing program ( ifstream and of stream )..
 
-   indata.close();
-   cout << "End-of-file reached.." << endl;
-
-
-   ifstream indata1;
-   string line;
-   int count=0;
-
-   indata1.open("test1.txt");
-
-    while (getline(indata1, line))
-    {
-        ++count;
-    }
-
-cout << "\n\n\nNumber of rows in text file: " << count	;
-cout << "\n\n\nNumber of cloumns in text file: " << (column/count)	;
-
-    indata1.close();
-
+   cout<<" modified code ( by deleting entire code ) "; 	
 
    return 0;
 }
